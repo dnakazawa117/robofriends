636df653842a49a0ae3f0c998ae807900236bceb
Denis Nakazawa <denisnakazawa@Deniss-MacBook-Pro.local>
2023-02-15 11:03:48 -0500

    updated package vulnerabilities and added error boundary

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from './Card.js';
 
 const CardList = ({ robots }) => {
+    if (true) {
+        throw new Error("Nooo!");
+    }
+    
     return (
         <>
         {
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import CardList from '../components/CardList.js';
+import ErrorBoundary from '../components/ErrorBoundary.js';
 import Scroll from '../components/Scroll.js';
 import SearchBox from '../components/SearchBox.js';
 
@@ -42,7 +43,9 @@ class App extends Component {
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
